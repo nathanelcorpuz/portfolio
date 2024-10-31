a35718762b34d62120e3622365a47b09db076228
natdlc <nathanelcorpuz@gmail.com>
2024-10-31 19:51:44 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,128 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	return (
+		<div className="max-w-[1000px]">
+			<header className="flex justify-between">
+				<p className="">Nate Corpuz</p>
+				<div className="flex gap-[20px]">
+					<p>Home</p>
+					<p>About</p>
+					<p>Projects</p>
+					<p>Contact</p>
+				</div>
+			</header>
+			<main className="">
+				<div>
+					<p className="text-2xl font-[family-name:var(--font-geist-mono)]">
+						Hi, I{"'"}m Nate.
+					</p>
+					<p>A full-stack web developer.</p>
+				</div>
+				<div>
+					<p className="text-2xl font-[family-name:var(--font-geist-mono)]">
+						About Me
+					</p>
+					<div>
+						<p>
+							Raised in a technological home by a computer engineer father with
+							an internet café business, my passion for software naturally
+							arose.
+						</p>
+						<p>
+							After working in customer service and virtual assistance for 11
+							years while coding on the side for 5 years, I am finally turning
+							my passion for coding into a career as a full-stack web developer.
+						</p>
+						<p>
+							Outside coding, I love going to the mall, morning walks, video
+							games, guitar, and drums.
+						</p>
+					</div>
+					<p className="text-2xl font-[family-name:var(--font-geist-mono)]">
+						My Tech Stack
+					</p>
+					<p>
+						React.js, Next.js, MongoDB, Vercel, TypeScript, TailwindCSS,
+						Postman, TanStack Query, Node.js, Visual Studio Code, Git/GitHub
+					</p>
+				</div>
+				<div>
+					<p className="text-2xl font-[family-name:var(--font-geist-mono)]">
+						Projects
+					</p>
+					<div>
+						<a href="https://orium.vercel.app" target="_blank">
+							Orium
+						</a>
+						<p>
+							An MVP web app that forecasts and organizes personal finances.
+						</p>
+						<p>Key Features</p>
+						<ul>
+							<li>Elegant UI</li>
+							<li>Auth workflows with nodemailer</li>
+							<li>Dashboard view</li>
+							<li>Settings and Preferences</li>
+							<li>Fully Typed</li>
+						</ul>
+						<p>Libraries</p>
+						<ul>
+							<li>Tanstack Query</li>
+							<li>TailwindCSS</li>
+							<li>TypeScript</li>
+							<li>date-fns</li>
+							<li>bcrypt</li>
+							<li>jsonwebtoken</li>
+							<li>mongoose</li>
+							<li>password-validator</li>
+						</ul>
+						<p>Architecture</p>
+						<ul>
+							<li>Next.js</li>
+							<li>Vercel</li>
+							<li>MongoDB</li>
+							<li>GitHub</li>
+						</ul>
+					</div>
+					<div>
+						<p>Mealer</p>
+						<p>A web app that organizes recipes and daily meals</p>
+						<p>Key Features</p>
+						<ul>
+							<li>Simple UI</li>
+							<li>Usage of loaders</li>
+							<li>Usage of route groups and dynamic routes</li>
+							<li>Fully typed</li>
+						</ul>
+						<p>Libraries</p>
+						<ul>
+							<li>Tanstack Query</li>
+							<li>TailwindCSS</li>
+							<li>TypeScript</li>
+							<li>bcrypt</li>
+							<li>jsonwebtoken</li>
+							<li>mongoose</li>
+							<li>password-validator</li>
+							<li>slugify</li>
+						</ul>
+						<p>Architecture</p>
+						<ul>
+							<li>Next.js</li>
+							<li>Vercel</li>
+							<li>MongoDB</li>
+							<li>GitHub</li>
+						</ul>
+					</div>
+				</div>
+				<div>
+					<p className="text-2xl font-[family-name:var(--font-geist-mono)]">
+						Contact Me
+					</p>
+					<p>I'd love to chat! You can reach me via nathanelcorpuz@gmail.com</p>
+				</div>
+			</main>
+			<footer className="">
+				<p>2024</p>
+			</footer>
+		</div>
+	);
 }
